c46e62ebf08ade76b6ba51e6a098f69d9d24a442
Mirjam Hachem <mirjam.hachem@gmail.com>
2017-05-07 22:51:12 +0200

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -6,7 +6,8 @@ power_consumption <- read.table("household_power_consumption.txt", header = TRUE
 # Subsetting the rows with the relevant dates: 1st and 2nd of February 2007
 subset_power_consumption <- power_consumption[power_consumption$Date %in% c("1/2/2007", "2/2/2007"),]
 
-# Creating the variable for the x-axis for all four plots by Converting the variables Date and Time from character strings into date and time formats and pasting them together.
+# Creating the variable for the x-axis for all four plots by Converting the variables Date and Time 
+# from character strings into date and time formats and pasting them together.
 date_time_merged <- strptime(paste(subset_power_consumption$Date, subset_power_consumption$Time, sep = " "), "%d/%m/%Y %H:%M:%S")
 
 # Creating the variable for the y-axis for the top left plot by storing Global_active_power in a numeric vector.
@@ -28,7 +29,6 @@ png("plot4.png", width=480, height= 480)
 
 # Creating a space with 2 column and 2 rows
 par(mfrow = c(2,2))
-
 
 # Creating the plots
 
