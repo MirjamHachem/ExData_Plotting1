ce3de9ad68e97a39500bc0e20958dce33095b82a
Mirjam Hachem <mirjam.hachem@gmail.com>
2017-05-07 21:12:26 +0200

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,19 @@
+# Reading the data set into R
+power_consumption <- read.table("household_power_consumption.txt", header = TRUE, sep =";",stringsAsFactors=FALSE, dec=".")
+
+# Subsetting the rows with the relevant dates: 1st and 
+# 2nd of February 2007.
+subset_power_consumption <- power_consumption[power_consumption$Date %in% c("1/2/2007", "2/2/2007"),]
+
+# Storing the variable Global_active_power in a numeric
+# vector
+Global_active_power <- as.numeric(subset_power_consumption$Global_active_power)
+
+# Creating the PNG file
+png("plot1.png", width=480, height=480)
+
+# Creating the histogram
+hist(Global_active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab= "Frequency", col = "red")
+
+# Closing the plotting procedure
+dev.off()
