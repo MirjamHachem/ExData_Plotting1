f94723ee07dae964d86f5b79220adc819d9b43f2
Mirjam Hachem <mirjam.hachem@gmail.com>
2017-05-07 21:14:48 +0200

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,12 +1,12 @@
-# Reading the data set into R
-power_consumption <- read.table("household_power_consumption.txt", header = TRUE, sep =";",stringsAsFactors=FALSE, dec=".")
+# This is the R script for the first plot Global Active Power
 
-# Subsetting the rows with the relevant dates: 1st and 
-# 2nd of February 2007.
+# Reading the data set into R
+power_consumption <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", stringsAsFactors = FALSE, dec = ".")
+
+# Subsetting the rows with the relevant dates: 1st and 2nd of February 2007
 subset_power_consumption <- power_consumption[power_consumption$Date %in% c("1/2/2007", "2/2/2007"),]
 
-# Storing the variable Global_active_power in a numeric
-# vector
+# Storing the variable Global_active_power in a numeric vector
 Global_active_power <- as.numeric(subset_power_consumption$Global_active_power)
 
 # Creating the PNG file
@@ -17,3 +17,5 @@ hist(Global_active_power, main = "Global Active Power", xlab = "Global Active Po
 
 # Closing the plotting procedure
 dev.off()
+
+# Thank you for grading my assignment! :-)
